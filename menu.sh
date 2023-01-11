2b84909414c8dc7da0e474892f7cc3cf624f83b6
Nicole <1530486@uab.cat>
2023-01-11 10:40:21 +0100

    Menù

---
+++ menu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "------------------------"
+echo "Base de dades de peliculas"
+echo "------------------------"
+echo "Elige una opcion"
+echo "1.Tarea 1.3"
+echo "2.Tarea 2.3"
+echo "3.Tarea 3.3"
+read var
+case $var in 
+1) #Llama a la funcion que hace la tarea 1
+2) #Llama a la funcion que hace la tarea 2
+3) #Llama a la funcion que hace la tarea 3
+
+esac
+
+
+
